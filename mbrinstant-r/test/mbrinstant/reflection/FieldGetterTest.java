47147a5f7cfbabdee8eeb6c125cab5334436486a
maine <mainevillarias@gmail.com>
2015-08-20 08:41:52 +0800

    field test

---
+++ mbrinstant-r/test/mbrinstant/reflection/FieldGetterTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mbrinstant.reflection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author maine
+ */
+public class FieldGetterTest {
+
+    public FieldGetterTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testFieldGetter() {
+
+    }
+}
